945c23cce07cb0b351aee194dade27dd4d27e676
German montero <germonram@gmail.com>
2026-04-28 19:05:44 -0600

    feat: Export Badge and Card components from package

--- packages/astro/index.ts
+++ packages/astro/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from './src/components/Button/Button.astro';
+export { default as Badge } from './src/components/Badge/Badge.astro';
+export { default as Card } from './src/components/Card/Card.astro';
+
