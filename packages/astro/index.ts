690d46544e67d7537d71a35325a81770930e542f
German montero <germonram@gmail.com>
2026-04-30 21:55:30 -0600

    Feat(astro): Export new form components

--- packages/astro/index.ts
+++ packages/astro/index.ts
@@ -6,4 +6,9 @@ export { default as Code } from './src/components/Code/Code.astro';
 export { default as Breadcrumb } from './src/components/Breadcrumb/Breadcrumb.astro';
 export { default as Tree } from './src/components/Tree/Tree.astro';
 export { default as Input } from './src/components/Input/Input.astro';
+export { default as FormField } from './src/components/FormField/FormField.astro';
+export { default as Form } from './src/components/Form/Form.astro';
+export { default as Select } from './src/components/Select/Select.astro';
+export { default as Checkbox } from './src/components/Checkbox/Checkbox.astro';
+export { default as Radio } from './src/components/Radio/Radio.astro';
 
