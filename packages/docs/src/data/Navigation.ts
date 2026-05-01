aab5744b57adb6a240e8376c05c58d20632454d8
German montero <germonram@gmail.com>
2026-04-30 19:48:00 -0600

    feat(docs): Implement new sidebar layout infrastructure

---
+++ packages/docs/src/data/Navigation.ts
@@ -0,0 +1,43 @@
+export const Navigation = [
+    {
+        label: 'Home',
+        href: '/',
+    },
+    {
+        label: 'Components',
+        children: [
+            {
+                label: 'Button',
+                href: '/button',
+            },
+            {
+                label: 'Badge',
+                href: '/badge',
+            },
+            {
+                label: 'Card',
+                href: '/card',
+            },
+            {
+                label: 'Alert',
+                href: '/alert',
+            },
+            {
+                label: 'Code',
+                href: '/code',
+            },
+            {
+                label: 'Breadcrumb',
+                href: '/breadcrumb',
+            },
+            {
+                label: 'Tree',
+                href: '/tree',
+            },
+            {
+                label: 'Input',
+                href: '/input',
+            },
+        ],
+    },
+];
---
+++ packages/docs/src/layouts/index.ts
@@ -0,0 +1,3 @@
+export { default as DocsLayout } from './DocsLayout.astro';
+export { default as HomeLayout } from './HomeLayout.astro';
+export { default as SideBarLayout } from './SideBarLayout.astro';
